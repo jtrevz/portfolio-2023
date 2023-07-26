6d4b2af3cc65f18102b6c53ed358ad0064cd9491
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-07-26 01:30:01 -0500

    responsiveness

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,12 +3,11 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import UpRight from "../../assets/icons/up-right";
-import aboutGradient from "../../assets/images/backgrounds/full-gradient.png";
 import "./styles.css";
 
 export default function About() {
   return (
-    <Parallax id="about" pages={2}>
+    <Parallax id="about" pages={2.5}>
       <ParallaxLayer factor={0.5}>
         <Container fluid>
           <Row className="f-row d-flex justify-content-end align-items-center pt-2 pt-4 p-sm-4 mb-5 mb-sm-0">
@@ -53,14 +52,14 @@ export default function About() {
         </Container>
       </ParallaxLayer>
       <ParallaxLayer offset={1} speed={0.5} factor={1.5}>
-        <Container fluid className="p-5">
-          <Row className="mt-md-5">
-            <h2 className="about-header px-5 pb-2">Experience</h2>
+        <Container fluid className="p-md-5 p-3">
+          <Row className="mt-md-5 my-3">
+            <h2 className="about-header px-md-5 pb-2">Experience</h2>
           </Row>
           <Row className="d-flex justify-content-end">
-            <Col className="col-12 col-md-9">
-              <Container fluid className="py-3 border">
-                <Row>
+            <Col className="col-12 col-md-10">
+              <Container fluid className="py-2 py-md-3 ">
+                <Row className="my-2">
                   <Col>
                     <h4 className="text-end px-4 profession-titles">
                       2022-2023
@@ -72,7 +71,7 @@ export default function About() {
                     </h4>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="mb-2">
                   <Col></Col>
                   <Col className="col-8">
                     <p>
@@ -86,7 +85,7 @@ export default function About() {
                     </p>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="my-2">
                   <Col>
                     <h4 className="text-end px-4 profession-titles">2024</h4>
                   </Col>
@@ -96,7 +95,7 @@ export default function About() {
                     </h4>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="mb-2">
                   <Col></Col>
                   <Col className="col-8">
                     <p>
@@ -110,12 +109,12 @@ export default function About() {
                     </p>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="my-3">
                   <Col>
                     <h3 id="edu-title">Education</h3>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="">
                   <Col>
                     <h4 className="text-end px-4 profession-titles">2021</h4>
                   </Col>
@@ -125,7 +124,7 @@ export default function About() {
                     </h4>
                   </Col>
                 </Row>
-                <Row>
+                <Row className="mb-2">
                   <Col></Col>
                   <Col className="col-8">
                     <p>University of Texas at Austin, EdX</p>
@@ -141,14 +140,14 @@ export default function About() {
                     </h4>
                   </Col>
                 </Row>
-                <Row className="">
+                <Row className="mb-2">
                   <Col></Col>
                   <Col className="col-8">
                     <p>University of Texas at Austin, EdX</p>
                   </Col>
                 </Row>
-                <Row className="d-flex justify-content-end m-4 my-5">
-                  <Button className=" d-block btn-warning resume-button">
+                <Row className="d-flex justify-content-end">
+                  <Button className="d-block btn-warning contact-button my-3 mx-md-4">
                     Resume
                   </Button>
                 </Row>
