e806462d7af15084ede679bf9bc2e30821e25a25
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-08-02 00:54:46 -0500

    contact layout responsiveness

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ContactNav from "../../components/ContactNav";
-import { Github, Linkedin, Pin, Pinterest } from "react-bootstrap-icons";
+import { Github, Linkedin, Pinterest } from "react-bootstrap-icons";
 import "./styles.css";
 
 export default function Contact() {
   return (
     <Container fluid id="contact" className="">
-      <Row className="d-flex m-0 pt-3">
-        <Col className="p-4">
+      <Row className="d-flex m-0 pt-3 pt-xxl-4">
+        <Col className="col-8 col-md-4 p-4 px-xxl-5">
           <h3 className="contact-name">Jennifer Trevizo</h3>
         </Col>
         <Col>
           <ContactNav />
         </Col>
       </Row>
-      <Row>
+      <Row className="px-xxl-4">
         <Col className="px-5">
           <h1 className="contact-heading">
             Get in
@@ -23,8 +23,8 @@ export default function Contact() {
           </h1>
         </Col>
       </Row>
-      <Row className="bottom p-5 mb-5 d-flex justify-content-between w-100">
-        <Col className="social-groups">
+      <Row className="bottom p-5 pe-sm-3 p-md-5 mb-5 mx-xxl-3 d-flex w-100 justify-content-between">
+        <Col className="social-groups col-12 col-sm-4 ps-sm-0 py-3">
           <h4 className="m-0">Email</h4>
           <h5 className="soc-links">
             <a href="mailto:jennytrevizo18@gmail.com">
@@ -32,29 +32,32 @@ export default function Contact() {
             </a>
           </h5>
         </Col>
-        <Col className="social-groups">
+        <Col className="social-groups col-12 col-sm-3 py-3">
           <h4 className="m-0">Phone</h4>
           <h5 className="soc-links">
             <a href="tel:+17133825761">713-382-5761</a>
           </h5>
         </Col>
-        <Col className="social-groups">
+        <Col className="social-groups col-12 col-sm-3 pt-3">
           <h4 className="m-0">Socials</h4>
           <h5 className="soc-links">
-            <a className="pe-2" href="https://github.com/jtrevz">
+            <a className="pe-2 pe-xxl-3" href="https://github.com/jtrevz">
               <span>
                 <Github size={20} />
               </span>
             </a>
             <a
-              className="px-2"
+              className="px-2 px-xxl-3"
               href="https://www.linkedin.com/in/jennifertrevizo/"
             >
               <span>
                 <Linkedin size={20} />
               </span>
             </a>
-            <a className="px-2" href="https://www.pinterest.com/jennytrevizo/">
+            <a
+              className="px-2 px-xxl-3"
+              href="https://www.pinterest.com/jennytrevizo/"
+            >
               <span>
                 <Pinterest size={20} />
               </span>
