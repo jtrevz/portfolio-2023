7127e4fcca02acef556caf8679738ace503549af
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-08-02 23:57:17 -0500

    project img responsive

--- src/assets/info/projects.js
+++ src/assets/info/projects.js
@@ -2,9 +2,12 @@ const ProjectData = [
   {
     name: "Studious",
     type: "Personal Project",
+    image: "https://i.imgur.com/2WkSqxE.jpg",
     description:
       "Developed and deployed a front end web application to help users make and study flashcards",
     tech: "React, Express, Node, Firebase",
+    github: "https://github.com/jtrevz/studious",
+    liveSite: "https://studious-2022.herokuapp.com",
   },
 ];
 
