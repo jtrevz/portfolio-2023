f455980d323ea4365bb5da960eab2c40102ca861
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-09-12 23:41:57 -0500

    parallax scrolling on work

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import HomeButton from "../../components/HomeButton";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Accordion from "../../components/Accordion/";
 
 import "./styles.css";
 
 export default function Work() {
   return (
-    <Container fluid id="work">
-      <HomeButton />
-      <Row className="wait-show d-flex justify-content-start px-5 mb-md-0 mb-5">
-        <h2 className="work-title">Work</h2>
-      </Row>
-      <Row className="d-flex justify-content-md-end justify-content-center mx-3">
-        <Accordion />
-      </Row>
-    </Container>
+    <Parallax id="work" pages={2}>
+      <ParallaxLayer factor={1.5} speed={0.5}>
+        <Container fluid>
+          <HomeButton />
+          <Row className="wait-show d-flex justify-content-start px-5 mb-md-0 mb-5">
+            <h2 className="work-title">Work</h2>
+          </Row>
+          <Row className="d-flex justify-content-md-end justify-content-center mx-3">
+            <Accordion />
+          </Row>
+        </Container>
+      </ParallaxLayer>
+    </Parallax>
   );
 }
