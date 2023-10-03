102b3d56d85ff217bf8f83fbb53441a495b19ee4
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-10-03 13:56:19 -0500

    new sections to autoforms

--- src/pages/AutoForm/index.js
+++ src/pages/AutoForm/index.js
@@ -1,50 +1,54 @@
 import React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useScroll, animated } from "@react-spring/web";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import contract from "../../assets/images/contractSample.png";
 import cardInput from "../../assets/images/cardInput.png";
+import layout from "../../assets/images/home-page.png";
 import "./styles.css";
 
 export default function AutoForm() {
   return (
-    <Parallax id="autoform" className="bg-light" pages={4}>
+    <Parallax id="autoform" className="bg-light" pages={10}>
       <ParallaxLayer sticky={{ start: 0, end: 10 }}>
         <Container fluid className="px-0">
           <Row>
             <Col
               id="auto-form-title"
-              md={6}
-              lg={5}
-              className="py-3 py-md-4 bg-danger"
+              xs={12}
+              className="py-3 py-md-3 bg-danger"
             >
-              <h1 className="bricolage text-center text-light">AutoForm</h1>
+              <h1 className="bricolage px-5 text-end text-light">AutoForm</h1>
             </Col>
           </Row>
           <Row></Row>
         </Container>
       </ParallaxLayer>
       <ParallaxLayer
-        speed={1}
-        factor={2}
-        offset={0.2}
-        sticky={{ start: 0.2, end: 1.5 }}
-        className="d-flex align-items-start align-items-md-center align-items-lg-end contract-parallax"
+        speed={0.6}
+        factor={1}
+        sticky={{ start: 0, end: 0.2 }}
+        className="d-flex align-items-md-center contract-parallax align-items-start"
       >
         <Container fluid>
           <Row>
-            <Col xs={12} lg={8} className="p-5 d-flex justify-content-center">
+            <Col
+              xs={8}
+              md={6}
+              lg={6}
+              className="ps-5 d-flex justify-content-center "
+            >
               <div className="contract-container">
                 <Image src={contract} fluid />
               </div>
             </Col>
           </Row>
-          <Row></Row>
         </Container>
       </ParallaxLayer>
-      <ParallaxLayer className=" s" speed={0.5} offset={0.2} factor={0.3}>
+      {/* <ParallaxLayer className="" speed={0.5} offset={0.4} factor={0.3}>
         <Container fluid>
-          <Row className="d-flex justify-content-end">
+          <Row className="d-none d-flex align-content-start justify-content-md-end">
             <Col xs={12} lg={5} className="">
               <div className="sub-box">
                 <h2 className="text-center sub-heading-a">
@@ -54,15 +58,58 @@ export default function AutoForm() {
             </Col>
           </Row>
         </Container>
-      </ParallaxLayer>
-      <ParallaxLayer className=" s" speed={0.5} offset={1} factor={0.3}>
+      </ParallaxLayer> */}
+      <ParallaxLayer className="" speed={0.5} offset={0.3} factor={0.2}>
         <Container fluid>
           <Row className="d-flex justify-content-end">
-            <Col xs={12} lg={5} className="">
+            <Col xs={12} lg={6} className="d-flex justify-content-center">
+              <div className="sub-box">
+                <h2 className="text-center sub-heading-a ">
+                  Autocomplete to fascilitate form creation
+                </h2>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </ParallaxLayer>
+      <ParallaxLayer className="" speed={0.4} offset={0.6} factor={0.2}>
+        <Container fluid>
+          <Row className="d-flex justify-content-md-end">
+            <Col xs={12} lg={6} className="d-flex justify-content-center">
+              <div className="sub-box">
+                <h2 className="text-center  text-secondary  sub-headings-c">
+                  Built for a client in construction. Allows them to easily
+                  create estimate forms without the hassle of having to format.
+                </h2>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </ParallaxLayer>
+      <ParallaxLayer
+        speed={1}
+        factor={1}
+        sticky={{ start: 1.2, end: 1.5 }}
+        className="d-flex align-items-center contract-parallax"
+      >
+        <Container fluid>
+          <Row className="d-flex  justify-content-md-end">
+            <Col xs={12} lg={7} className="p-5 d-flex justify-content-center">
+              <div className="contract-container">
+                <Image src={cardInput} fluid />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </ParallaxLayer>
+
+      <ParallaxLayer className="" speed={0.4} offset={1.8} factor={0.3}>
+        <Container fluid>
+          <Row className="d-flex">
+            <Col xs={12} lg={7} className=" d-flex justify-content-center">
               <div className="sub-box">
                 <h2 className="text-center sub-headings-b">
-                  Built for a client in construction that allows them to easily
-                  create estimate forms without the hassle of having to format.
+                  Generates autocomplete options based on past user input
                 </h2>
               </div>
             </Col>
@@ -71,29 +118,43 @@ export default function AutoForm() {
       </ParallaxLayer>
 
       <ParallaxLayer
-        speed={0.3}
+        speed={1}
         factor={1}
-        sticky={{ start: 2.5, end: 3 }}
-        className="d-flex align-items-center contract-parallax"
+        sticky={{ start: 2.8, end: 3.5 }}
+        className="d-flex align-items-md-center contract-parallax align-items-start"
       >
         <Container fluid>
-          <Row className=" d-flex justify-content-end">
-            <Col xs={12} lg={7} className="p-5 d-flex justify-content-center">
-              <div className="contract-container">
-                <Image src={cardInput} fluid />
+          <Row>
+            <Col xs={7} className="ps-5 d-flex justify-content-center ">
+              <div className="contract-container-home">
+                <Image src={layout} fluid />
               </div>
             </Col>
           </Row>
           <Row></Row>
         </Container>
       </ParallaxLayer>
-      <ParallaxLayer className=" s" speed={0.5} offset={3} factor={0.3}>
+      <ParallaxLayer className="" speed={0.5} offset={3} factor={0.3}>
         <Container fluid>
-          <Row className="d-flex">
-            <Col xs={12} lg={7} className=" d-flex justify-content-center">
+          <Row className="d-flex justify-content-md-end">
+            <Col xs={12} lg={6} className="d-flex justify-content-center">
               <div className="sub-box">
                 <h2 className="text-center sub-headings-b">
-                  Generates autocomplete options based on past user input
+                  Past contracts saved and displayed in home page.
+                </h2>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </ParallaxLayer>
+      <ParallaxLayer className="" speed={0.4} offset={3.2} factor={0.2}>
+        <Container fluid>
+          <Row className="d-flex justify-content-md-end">
+            <Col xs={12} lg={6} className="d-flex justify-content-center">
+              <div className="sub-box">
+                <h2 className="text-center sub-headings-c">
+                  Ability to view, or download contracts directly from home
+                  page.
                 </h2>
               </div>
             </Col>
